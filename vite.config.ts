6a97161ba7452371a4a8ff5e5b420e5d8ac25043
AXCWG <xiey0@qq.com>
2025-11-04 16:24:19 +0800

    ManifestFix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 import tailwindcss from "@tailwindcss/vite";
+import {webmanifestPlugin} from "@budarin/vite-plugin-webmanifest";
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss()],
+  plugins: [devtools(), solidPlugin(), tailwindcss(), webmanifestPlugin()],
   server: {
     port: 3000,
   },
