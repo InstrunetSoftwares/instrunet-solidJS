794ebca4a99b492e763e085ec8029b0766bfb563
AXCWG <xiey0@qq.com>
2026-01-09 23:46:15 +0800

    Optimize for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 import tailwindcss from "@tailwindcss/vite";
 import {webmanifestPlugin} from "@budarin/vite-plugin-webmanifest";
+import {viteSingleFile} from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss(), webmanifestPlugin()],
+  plugins: [devtools(), solidPlugin(), tailwindcss(), webmanifestPlugin(), viteSingleFile()],
   server: {
     port: 3000,
   },
