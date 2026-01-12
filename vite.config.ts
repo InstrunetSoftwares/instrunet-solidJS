04af9d3f3d53e748d9438ddaddb4d073908ead29
AXCWG <xiey0@qq.com>
2026-01-12 13:46:23 +0800

    remove manifest until the guy fixes it

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,17 @@ import devtools from 'solid-devtools/vite';
 import tailwindcss from "@tailwindcss/vite";
 import {viteSingleFile} from "vite-plugin-singlefile";
 import htmlMinifier from "vite-plugin-html-minifier";
+import {webmanifestPlugin} from "@budarin/vite-plugin-webmanifest";
 
 export default defineConfig({
+	base: "/",
   plugins: [devtools(), solidPlugin(), tailwindcss(),viteSingleFile({
 	  overrideConfig: {
 		  base: "/"
 	  }
   }), htmlMinifier({
 	  minify: true
-  }), ],
+  }) ],
   server: {
     port: 3000,
   },
